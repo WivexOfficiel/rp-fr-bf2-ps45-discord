0c0f9228788d00aac8918a3eae5f1c091fb8f113
Wivex <136355691+WivexOfficiel@users.noreply.github.com>
2024-06-16 17:18:00 +0200

    Update display_black_list_test.py

--- display_black_list_test.py
+++ display_black_list_test.py
@@ -1,17 +1,32 @@
 import os
 
+players_list_directory = "players_list"
+
+def read_file_with_fallback(filepath):
+    encodings = ['utf-8', 'ISO-8859-1', 'latin-1']
+    for encoding in encodings:
+        try:
+            with open(filepath, 'r', encoding=encoding) as file:
+                return file.readlines()
+        except UnicodeDecodeError:
+            continue
+    raise UnicodeDecodeError(f"Erreur de décodage pour le fichier : {filepath}")
+
 def display_blacklisted_players():
-    # Liste pour stocker les informations des joueurs blackliste
+    # Liste pour stocker les informations des joueurs blacklistés
     blacklisted_players = []
 
     # Parcourir tous les fichiers dans le dossier players_list
-    for filename in os.listdir("players_list"):
+    for filename in os.listdir(players_list_directory):
         if filename.endswith(".txt"):
-            player_filepath = os.path.join("players_list", filename)
+            player_filepath = os.path.join(players_list_directory, filename)
             
-            # Lire les informations du joueur
-            with open(player_filepath, 'r') as file:
-                lines = file.readlines()
+            # Lire les informations du joueur avec gestion de plusieurs encodages
+            try:
+                lines = read_file_with_fallback(player_filepath)
+            except UnicodeDecodeError as e:
+                print(e)
+                continue
 
             # Vérifier si le joueur est sur la blacklist
             is_blacklisted = False
@@ -25,7 +40,7 @@ def display_blacklisted_players():
                 player_info = ''.join(lines)
                 blacklisted_players.append(player_info)
     
-    # Afficher les informations des joueurs blackliste
+    # Afficher les informations des joueurs blacklistés
     if blacklisted_players:
         print("Joueurs sur la blacklist :")
         for player in blacklisted_players:
